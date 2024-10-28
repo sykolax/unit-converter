5b8730a9b53fffc21aefd5636b431522fe48bf27
syko <sykolax@gmail.com>
2024-10-28 21:25:33 +0900

    feat: get necessary elements from html

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const LITER2GALLON = 0.264172;
 const GALLON2LITER = 3.78541;
 const KG2POUND = 2.20462;
 const POUND2KG = 0.453592;
+
+const convertBtn = document.querySelector("#convert-btn");
+const inputEl = document.querySelector("input[type='number']");
+const lengthTextEl = document.querySelector("#length-conversion p");
+const volumeTextEl = document.querySelector("#volume-conversion p");
+const massTextEl = document.querySelector("#mass-conversion p");
